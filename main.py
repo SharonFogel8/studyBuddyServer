863d5390099b3d6612e7d90d001fb144ba9d39b1
sharonfogel8 <sharonfogel95@gmail.com>
2024-05-26 13:09:46 +0300

    new one

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import streamlit as st
-import os
-import logging
 from dotenv import load_dotenv
-from gui import ui
-from modules import buttons_actions
-from modules import data_manager
+# from gui import ui
+from modules import buttons_actions, ui
 from gui.htmlTemplates import css
 from pages.login_page import (login, cookies)
 from Objects.user_object import user
-from pages import login_page
-import define
+
 
 
 def main():
-    st.set_page_config(page_title="Study Buddy", page_icon=":books:")
+    if "set_page" not in st.session_state:
+        st.set_page_config(page_title="Study Buddy", page_icon=":books:")
+        st.session_state.set_page = 1
     print('main')
     load_dotenv()
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     login()
-    # print(st.session_state.username)
-    # if not cookies.get():
-    my_user = user(name=cookies["username"], uid=cookies["unique_key"], mail='cookies["email"]')
-    buttons_actions.create_process_button(my_user)
+    if "username" in cookies:
+        if user not in st.session_state:
+            my_user = user(name=cookies["username"], uid=cookies["unique_key"], mail='cookies["email"]')
+            st.session_state.user = my_user
+        else:
+            my_user = st.session_state.user
+        with st.sidebar:
+            buttons_actions.new_chat_button(my_user)
+            ui.sidebar_chat_history(my_user)
+        buttons_actions.create_process_button(my_user)
 
     st.write(css, unsafe_allow_html=True)
 
 
-    with st.sidebar:
-        buttons_actions.new_chat_button(my_user)
-        ui.sidebar_chat_history(my_user.uid)
+
         # data_manager.import_history_file()
 
 #משימות שלא סיימתי : להחליט אם להשאיר את chats כlist או להפוך את זה לhash map ואז כששומרים שיחה צריך להחליט איך להגיד לו לאיזה שיחה להשתייך
